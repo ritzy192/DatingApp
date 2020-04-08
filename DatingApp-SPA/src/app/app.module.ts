8bc6480035f5429af03ad24f55e37197c453f6ee
Rythem Datta <ritzy192@gmail.com>
2020-04-08 21:32:00 +0530

    Parent(Home) to child(Register) component data transfer example

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from './_service/auth.service';
@@ -12,7 +11,6 @@ import { RegisterComponent } from './register/register.component';
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent,
       NavComponent,
       HomeComponent,
       RegisterComponent
--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -19,6 +19,7 @@ namespace DatingApp.API.Controllers
         {
             _context = context;
         }
+        [AllowAnonymous]
         // GET api/values
         [HttpGet]
         public async Task<IActionResult> GetValues()
