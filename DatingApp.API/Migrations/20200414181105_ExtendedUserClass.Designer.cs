48f50fc3bea8c4ad323615ab73d2444139e3019f
Rythem Datta <ritzy192@gmail.com>
2020-04-15 00:01:23 +0530

    updated entity relationship bw photos and user model. ie cascade delete: Deleting user will delete photos of thst user.

--- DatingApp.API/Migrations/20200414181105_ExtendedUserClass.Designer.cs
+++ DatingApp.API/Migrations/20200414181105_ExtendedUserClass.Designer.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DatingApp.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20200414174345_ExtendedUserClass")]
+    [Migration("20200414181105_ExtendedUserClass")]
     partial class ExtendedUserClass
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -36,7 +36,7 @@ namespace DatingApp.API.Migrations
                     b.Property<string>("Url")
                         .HasColumnType("TEXT");
 
-                    b.Property<int?>("UserId")
+                    b.Property<int>("UserId")
                         .HasColumnType("INTEGER");
 
                     b.HasKey("Id");
@@ -112,9 +112,11 @@ namespace DatingApp.API.Migrations
 
             modelBuilder.Entity("DatingApp.API.Models.Photo", b =>
                 {
-                    b.HasOne("DatingApp.API.Models.User", null)
+                    b.HasOne("DatingApp.API.Models.User", "User")
                         .WithMany("MyProperty")
-                        .HasForeignKey("UserId");
+                        .HasForeignKey("UserId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
                 });
 #pragma warning restore 612, 618
         }
