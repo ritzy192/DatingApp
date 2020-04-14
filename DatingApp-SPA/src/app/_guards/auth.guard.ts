5fb3e5f375bf3bcac125a22e16a46f0c221a3f96
Rythem Datta <ritzy192@gmail.com>
2020-04-14 11:28:24 +0530

    implemented routing in angular

---
+++ DatingApp-SPA/src/app/_guards/auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router} from '@angular/router';
+import { AuthService } from '../_service/auth.service';
+import { AlertifyService } from '../_service/Alertify.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private alertify: AlertifyService
+  ) {}
+
+  canActivate(): boolean {
+    if (this.authService.loggedIn()) {
+      return true;
+    }
+    this.alertify.error('Invalid Path!!!');
+    this.router.navigate(['/home']);
+  }
+}
