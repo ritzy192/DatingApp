1a4336692f8d96b1f6915937824eb3c9b86c8702
Rythem Datta <ritzy192@gmail.com>
2020-04-17 18:14:40 +0530

    tested user service displaying name in member-list component

--- DatingApp-SPA/src/app/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/member-list/member-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../_models/User';
+import { UserService } from '../_service/user.service';
+import { AlertifyService } from '../_service/Alertify.service';
 
 @Component({
   selector: 'app-member-list',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-
-  constructor() { }
+  users: User[];
+  constructor(private userSerevice: UserService, private alertify: AlertifyService) { }
 
   ngOnInit() {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.userSerevice.getUsers().subscribe((users: User[]) => {
+      this.users = users;
+    }, error => {
+      this.alertify.error(error);
+    });
   }
 
 }
