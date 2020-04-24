3ca6e7a613db0bc6e436319ae54c643f3cb83c7a
Rythem Datta <ritzy192@gmail.com>
2020-04-24 23:13:41 +0530

    Added photo gallery using ngx-gallery package On member detail component

--- DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
+++ DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -3,6 +3,7 @@ import { User } from 'src/app/_models/User';
 import { UserService } from 'src/app/_service/user.service';
 import { AlertifyService } from 'src/app/_service/Alertify.service';
 import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
 
 @Component({
   selector: 'app-member-detail',
@@ -11,6 +12,9 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MemberDetailComponent implements OnInit {
   user: User;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
+
   constructor(private userService: UserService, private alertify: AlertifyService,
     // tslint:disable-next-line: align
     private route: ActivatedRoute) { }
@@ -20,6 +24,31 @@ export class MemberDetailComponent implements OnInit {
       // tslint:disable-next-line: no-string-literal
       this.user = data['user'];
     });
+
+    this.galleryOptions =[
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ];
+    this.galleryImages = this.getImages();
+  }
+
+  getImages() {
+    const imageUrls = [];
+    for (const photo of this.user.photos) {
+      imageUrls.push({
+        small: photo.url,
+        medium: photo.url,
+        big: photo.url,
+        description: photo.description
+      });
+    }
+    return imageUrls;
   }
 
   // loadUser() {
