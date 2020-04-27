bbac9dcf68b270f7efd631eba80528ef76708fae
Rythem Datta <ritzy192@gmail.com>
2020-04-27 22:53:02 +0530

    deactivated route before saving changes for member edit component

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -24,6 +24,7 @@ import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 export function tokenGetter() {
    return localStorage.getItem('token');
@@ -72,6 +73,7 @@ export class CustomHammerConfig extends HammerGestureConfig {
       AlertifyService,
       MemberDetailResolver,
       MemberEditResolver,
+      PreventUnsavedChanges,
       MemberListResolver,
       { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig }
    ],
